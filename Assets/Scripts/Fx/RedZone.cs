2ecd830110f7c2c0933026bc4cd783f2b83d129f
Ndd <duonghumg145@gmail.com>
2023-10-31 21:48:03 +0700

    add fx red zone

---
+++ Assets/Scripts/Fx/RedZone.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedZone : MonoBehaviour {
+	public void SetUp(float radious){
+		transform.localScale = new Vector3 (radious, radious, 1);
+	}
+}
